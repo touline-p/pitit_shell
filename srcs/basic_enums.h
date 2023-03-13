bb2d9462bc93f96324fd81d2ec4361e412846507
bpoumeau <delafforest.brieuc@gmail.com>
2023-03-13 22:24:38 +0100

    makefile seems good

---
+++ srcs/basic_enums.h
@@ -0,0 +1,16 @@
+//
+// Created by bpoumeau on 3/13/23.
+//
+
+#ifndef T_RET_STATUS
+# define T_RET_STATUS
+
+typedef	enum e_ret_status
+{
+	SUCCESS,
+	FAILURE,
+	MLC_ERR,
+	WRT_ERR
+}	t_ret_status;
+
+#endif
---
+++ srcs/expands/expands.c
@@ -0,0 +1,6 @@
+#include "expands.h"
+#include "basic_enums.h"
+
+t_ret_status
+{
+}
