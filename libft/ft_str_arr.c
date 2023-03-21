a9e0ebbca359e7c30472bad38512b29760271d4e
bpoumeau <delafforest.brieuc@gmail.com>
2023-03-21 20:49:20 +0100

    ft_str array join

--- libft/ft_str_arr.c
+++ libft/ft_str_arr.c
@@ -1,8 +1,8 @@
 #include "libft.h"
 
-static char **_ft_str_arr_dup(char **str_arr, size_t i);
+static char **_ft_str_array_dup_ep(char **str_arr, size_t i);
 
-size_t	ft_str_arr_len(char **str_arr)
+size_t	ft_str_array_len(char **str_arr)
 {
 	size_t	i;
 
@@ -15,12 +15,12 @@ size_t	ft_str_arr_len(char **str_arr)
 	return (i);
 }
 
-char	**ft_str_arr_dup(char **str_arr)
+char	**ft_str_array_dup(char **str_arr)
 {
 	char	**new;
 	size_t	i;
 
-	new = malloc(sizeof(char *) * (ft_str_arr_len(str_arr) + 1));
+	new = malloc(sizeof(char *) * (ft_str_array_len(str_arr) + 1));
 	if (new == NULL)
 		return (NULL);
 	i = 0;
@@ -28,18 +28,36 @@ char	**ft_str_arr_dup(char **str_arr)
 	{
 		new[i] = ft_strdup(*str_arr);
 		if (new[i] == NULL)
-			return (_ft_str_arr_dup_ep(new, ++i));
+			return (_ft_str_array_dup_ep(new, ++i));
 		i++;
 		str_arr++;
 	}
-	new[i] i= NULL;
+	new[i] = NULL;
 	return (new);
 }
 
-static char **_ft_str_arr_dup(char **str_arr, size_t i)
+static char **_ft_str_array_dup_ep(char **str_arr, size_t i)
 {
 	while (i--)
 		free(str_arr[i]);
 	free(str_arr);
 	return (NULL);
+}
+
+char **ft_strarray_join(char **first, char **second)
+{
+	char **new;
+	char **pin;
+
+	new = malloc(sizeof(char *) * (ft_str_array_len(first) \
+			+ ft_str_array_len(second) + 1));
+	if (new == NULL)
+		return (NULL);
+	pin = new;
+	while (*first)
+		*(pin++) = *(first++);
+	while (*second)
+		*(pin++) = *(second++);
+	*pin = NULL;
+	return (new);
 }
