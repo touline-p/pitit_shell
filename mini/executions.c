aca8a6b83983712987ac85c56ed32003aa1d5388
Thea Wang <theawang@yahoo.com>
2023-03-27 16:02:17 +0200

    test adding executions

--- mini/executions.c
+++ mini/executions.c
@@ -6,50 +6,9 @@
 /*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 11:05:09 by twang             #+#    #+#             */
-/*   Updated: 2023/03/27 13:09:04 by twang            ###   ########.fr       */
+/*   Updated: 2023/03/27 14:59:06 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_parsing.h"
 
-/*---- prototypes ------------------------------------------------------------*/
-
-static void	token_recognition(t_string_token *string_of_tokens);
-
-/*----------------------------------------------------------------------------*/
-
-void	execution(t_string_token *string_of_tokens)
-{
-	token_recognition(string_of_tokens);
-}
-
-static void	token_recognition(t_string_token *string_of_tokens)
-{
-	t_string_token	*temp;
-
-	temp = string_of_tokens;
-	while (temp != NULL)
-	{
-		if (temp->token == START)
-			puts("start");
-		if (temp->token == PIPE)
-			puts("pipe");
-		if (temp->token == CHEVRON_IN)
-			puts("chevron_in");
-		if (temp->token == CHEVRON_OT)
-			puts("chevron_out");
-		if (temp->token == HERE_DOC)
-			puts("here_doc");
-		if (temp->token == APPENDS)
-			puts("appends");
-		puts("passe a la casse");
-		temp = temp->next;
-	}
-}
-	/// balader dans la liste chainees et faire en fonction
-	//create structure init -> fds management	-> fork management -> heredoc 
-	//check des chevrons	-> infile / outifile - les gerer - les virer
-	//check here_docs 		-> here_doc becomes infile 
-	//check des pipes		-> checks fds -> prepare for fork
-	//check commands 		-> builtins -> call built_in function
-	//						-> commands to exec -> get_path, ... pipex
