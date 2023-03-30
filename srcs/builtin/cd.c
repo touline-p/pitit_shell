576dc884a6355459e8d5243821150c8831a802f7
bpoumeau <delafforest.brieuc@gmail.com>
2023-03-30 05:32:05 +0200

    comportement de base sans update de l'env

---
+++ srcs/builtin/cd.c
@@ -0,0 +1,30 @@
+#include "../../libft/libft.h"
+#include "../../incs/parsing_incs/minishell_parsing.h"
+
+t_return_status	cd_builtin(char **av, char ***env_pt)
+{
+	if (ft_str_array_len(av) > 2)
+		return (dprintf(2, "cd: too much arguments\n"), ft_free_split(av), SUCCESS);
+//	if (ft_str_array_len(av) == 1)
+//		return (_go_to_home(env_pt));
+	chdir(av[1]);
+	if (errno)
+	{
+		perror(av[1]);
+		return (FAILURE);
+	}
+	return (SUCCESS);
+}
+
+int main(int ac, char **av, char **env)
+{
+	(void)ac; (void)av; (void)env;
+
+	env = ft_str_array_dup(env);
+	av = ft_str_array_dup(av);
+	puts(getcwd(NULL, 0));
+	cd_builtin(av, &env);
+	puts(getcwd(NULL, 0));
+	ft_free_split(env);
+	return (0);
+}
