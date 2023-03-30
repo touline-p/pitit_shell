c82ac5c2aa272f2b6a3cc995609639e86317f783
bpoumeau <delafforest.brieuc@gmail.com>
2023-03-30 05:02:46 +0200

    this seems right tested and approved by meeeeeeee

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -1,9 +1,46 @@
 #include "../../libft/libft.h"
 #include "../../incs/parsing_incs/minishell_parsing.h"
 
+static	bool	_is_option_n(char *arg);
+
+t_return_status	echo_builtin(char **av, char ***env_pt)
+{
+	char **tmp;
+	bool option;
+
+	tmp = av + 1;
+	option = _is_option_n(*tmp);
+	if (option == true)
+		tmp++;
+	if (*tmp)
+	{
+		printf("%s", *tmp);
+		tmp++;
+	}
+	while (*tmp)
+	{
+		printf(" %s", *tmp);
+		tmp++;
+	}
+	if (option == false)
+		printf("\n");
+	return (SUCCESS);
+}
+
+static	bool	_is_option_n(char *arg)
+{
+	if (arg == NULL || *(arg) != '-' || *(arg + 1) != 'n')
+		return (false);
+	arg++;
+	while (*arg == 'n')
+		arg++;
+	if (*arg)
+		return (false);
+	return (true);
+}
 
 
-#ifdef TST_ENV
+#ifdef TST_ECHO
 
 int	main(int ac, char **av, char **env)
 {
