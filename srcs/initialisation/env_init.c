396ed76aa63f2a607410c8d1d530399abac9a416
bpoumeau <delafforest.brieuc@gmail.com>
2023-03-30 07:05:53 +0200

    env init seems legit

--- srcs/initialisation/env_init.c
+++ srcs/initialisation/env_init.c
@@ -1,9 +1,10 @@
 #include "../../libft/libft.h"
 #include "../../incs/parsing_incs/minishell_parsing.h"
 
-static char *_get_cwd_var(void);
+static char				*_get_cwd_var(void);
 static t_return_status	_reconstruct_env(char ***env_pt);
 static t_return_status	_increment_shlvl(char ***env_pt);
+static int				_get_shlvl_nb(char *str);
 
 t_return_status	env_init_on(char ***env_pt)
 {
@@ -20,7 +21,7 @@ t_return_status	env_init_on(char ***env_pt)
 		line_cwd = _get_cwd_var();
 		if (line_cwd == NULL)
 			return (FAILED_MALLOC);
-		add_str_to_env(line_cwd);
+		add_str_to_env(line_cwd, env_pt);
 	}
 	return (SUCCESS);
 }
@@ -39,24 +40,40 @@ static t_return_status	_increment_shlvl(char ***env_pt)
 			return (FAILED_MALLOC);
 		return (SUCCESS);
 	}
-	shlvl = ft_atoi(ft_strchr(line, '=') + 1);
+	shlvl = _get_shlvl_nb(ft_strchr(line, '=') + 1);
 	line_to_add = ft_strjoin_free_sec_sf("SHLVL=", ft_itoa(shlvl + 1));
 	if (line_to_add == NULL)
 		return (FAILED_MALLOC);
-	replace_content_in_env(line, env_pt);
+	replace_content_in_env(line_to_add, *env_pt);
 	return (SUCCESS);
+}
+
+static int	_get_shlvl_nb(char *str)
+{
+	char *pin;
+
+	pin = str;
+	while (*pin)
+	{
+		if (*pin < '0' || *pin > '9')
+			return (0);
+		pin++;
+	}
+	return (ft_atoi(str));
 }
 
 static t_return_status	_reconstruct_env(char ***env_pt)
 {
 	char **new_env;
 
+	printf("here\n");
 	new_env = malloc(sizeof(char *) * 4);
 	if (new_env == NULL)
 		return (FAILED_MALLOC);
 	new_env[0] = ft_strdup("OLDPWD");
 	new_env[1] = _get_cwd_var();
 	new_env[2] = ft_strdup("SHLVL=\"1\"");
+	new_env[3] = NULL;
 	if (new_env[0] == NULL || new_env[1] == NULL || new_env[2] == NULL)
 		return (free(new_env[0]), free(new_env[1]), free(new_env[2]), free(new_env), FAILED_MALLOC);
 	*env_pt = new_env;
@@ -70,11 +87,11 @@ static char *_get_cwd_var(void)
 	size_t	len;
 
 	cwd = getcwd(NULL, 0);
-	dst = malloc(ft_strlen(cwd) + 5);
+	dst = malloc(ft_strlen(cwd) + 7);
 	if (dst == NULL || cwd == NULL)
 		return (free(cwd), free(dst), NULL);
 	ft_strcpy(dst, "PWD=");
-	ft_strcpy(dst + 5, cwd);
+	ft_strcpy(dst + 4, cwd);
 	len = ft_strlen(cwd) + 5;
 	dst[len] = 0;
 	free(cwd);
@@ -83,13 +100,13 @@ static char *_get_cwd_var(void)
 
 #ifdef  TST_GET_CMD_VAR
 
-int main()
+int main(int ac, char **av, char **env)
 {
 	char *line;
-
-	line = _get_cwd_var();
-	printf("%s\n", line);
-	free(line);
+	(void)ac; (void)av; (void)env;
+	env_init_on(&env);
+	ft_print_split(env);
+	ft_free_split(env);
 }
 
 #endif
