7b9f5334d26e8beb44e7e8dd5693fe94d1d1597a
bpoumeau <delafforest.brieuc@gmail.com>
2023-03-30 04:35:30 +0200

    testing ft done

---
+++ srcs/builtin/env.c
@@ -0,0 +1,26 @@
+#include "../../libft/libft.h"
+#include "../../incs/parsing_incs/minishell_parsing.h"
+
+t_return_status	env_builtin(char **av, char ***env_pt)
+{
+	char **pin;
+
+	ft_free_split(av)
+}
+
+
+#ifdef TST_ENV
+
+int	main(int ac, char **av, char **env)
+{
+	(void)ac; (void)av; (void)env;
+
+	env = ft_str_array_dup(env);
+	av = ft_str_array_dup(av);
+	env_builtin(av, &env);
+	ft_print_split(env);
+	ft_free_split(env);
+	return (0);
+}
+
+#endif
