d5af6aeb76465c06b2c860878301ebde3d5bd3be
bpoumeau <delafforest.brieuc@gmail.com>
2023-03-30 04:28:35 +0200

    pwd tested

---
+++ srcs/builtin/pwd.c
@@ -0,0 +1,30 @@
+#include "../../libft/libft.h"
+#include "../../incs/parsing_incs/minishell_parsing.h"
+
+t_return_status	pwd_builtin(char **args, char ***env_pt)
+{
+	char *pwd;
+
+	(void)env_pt;
+	ft_free_split(args);
+	pwd = getcwd(NULL, 0);
+	if (pwd == NULL)
+		return (dprintf(2, "Bro j'ai pas trouve dsl, tu es perdu :(\n"), FAILURE);
+	printf("%s\n", pwd);
+	free(pwd);
+
+	return (SUCCESS);
+}
+
+#ifdef TST_PWD
+
+int	main(int ac, char **av, char **env)
+{
+	av = ft_str_array_dup(av);
+	env = ft_str_array_dup(env);
+	pwd_builtin(av, &env);
+	ft_free_split(env);
+	return (0);
+}
+
+#endif
