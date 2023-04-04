20a0a0c3dc9a72ac70fc7554a0eaa05f3baef21f
bpoumeau <delafforest.brieuc@gmail.com>
2023-04-04 23:51:51 +0200

    c'est un peu le bordele mais ca me semble bon

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -4,7 +4,9 @@
 static t_return_status	_export_display(char **env);
 static void				_display_unic_export(char *env_line);
 
-void	modify_env(char *key, char **args, char **env);
+typedef t_return_status (*t_export_ft)(char *arg, char ***env_pt);
+
+t_export_ft	_get_ft_to_do(char *line, char **env);
 
 t_return_status	export_builtin(char **args, char ***env_pt)
 {
@@ -15,20 +17,76 @@ t_return_status	export_builtin(char **args, char ***env_pt)
 	tmp = args + 1;
 	while (*tmp != NULL)
 	{
-		if (has_a_key_from_env(*tmp, *env_pt) == true
-			&& ft_strchr(*tmp, '=') == NULL)
-			;
-		else if (has_a_key_from_env(*tmp, *env_pt) == true)
-			replace_content_in_env(*tmp, *env_pt);
-		else
-		{
-			if (add_str_to_env(*tmp, env_pt) != SUCCESS)
-				return (free(*args), free(args), FAILED_MALLOC);
-		}
+		if ((*_get_ft_to_do(*tmp, *env_pt))(*tmp, env_pt) != SUCCESS)
+			return (free(*args), ft_free_split(tmp), FAILED_MALLOC);
 		tmp++;
 	}
 	return (free(*args), free(args), SUCCESS);
 }
+
+
+void _get_rid_of_plus(char *line)
+{
+	if (line == NULL)
+		return ;
+	while (*line != '=' && *line)
+	{
+		if (*line == '+')
+		{
+			ft_memmove(line, line + 1, ft_strlen(line));
+			return ;
+		}
+		line++;
+	}
+}
+
+t_return_status replace_content_in_env_pt(char *line, char ***env_pt)
+{
+	return (replace_content_in_env(line, *env_pt), SUCCESS);
+}
+
+t_return_status do_nothing_t_export_ft(char *line, char ***env_pt)
+{
+	(void)line;
+	(void)env_pt;
+	return (SUCCESS);
+}
+
+t_return_status concat_content_to_line_in_env(char *line, char ***env_pt)
+{
+	char	*content;
+	char	**env_line;
+	char 	*tmp;
+
+	content = ft_strchr(line, '=');
+	*(content++) = 0;
+	env_line = get_line_addr_from_key(line, *env_pt);
+	tmp = *env_line;
+	*env_line = ft_strjoin(*env_line, content);
+	free(tmp);
+	free(line);
+	return (SUCCESS);
+}
+
+t_export_ft	_get_ft_to_do(char *line, char **env)
+{
+	char *eq;
+	char *plus;
+
+	eq = ft_strchr(line, '=');
+	plus = ft_strchr(line, '+');
+	_get_rid_of_plus(line);
+	if (has_a_key_from_env(line, env) == false)
+	{
+		return (&add_str_to_env);
+	}
+	if (eq == NULL)
+		return (&do_nothing_t_export_ft);
+	if (plus == eq - 1)
+		return (&concat_content_to_line_in_env);
+	return (&replace_content_in_env_pt);
+}
+
 
 
 static t_return_status	_export_display(char **env)
