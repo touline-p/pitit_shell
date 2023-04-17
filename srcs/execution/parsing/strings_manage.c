ee6623fa11e1e616c6242c917bdd669d654fab9e
Thea Wang <theawang@yahoo.com>
2023-04-17 19:40:21 +0200

    small organisation change

--- srcs/execution/parsing/strings_manage.c
+++ srcs/execution/parsing/strings_manage.c
@@ -6,7 +6,7 @@
 /*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 18:41:57 by twang             #+#    #+#             */
-/*   Updated: 2023/04/17 18:26:34 by twang            ###   ########.fr       */
+/*   Updated: 2023/04/17 19:39:52 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 
 static t_return_status	get_raw_cmds(t_data *data, t_string_token *lst_of_tok);
 static void				set_id_cmds(t_data *data);
-/*static t_return_status	add_path_cmd(int block_id, t_data *data, char **env);*/
 
 /*----------------------------------------------------------------------------*/
 
@@ -61,37 +60,3 @@ static void	set_id_cmds(t_data *data)
 		block_id++;
 	}
 }
-
-/*
-static t_return_status	add_path_cmd(int block_id, t_data *data, char **env)
-{
-	int		i;
-	char	**paths;
-
-	if (access(data->cmds_block[block_id].commands[0], X_OK) == 0)
-		return (SUCCESS);
-	paths = get_paths(env);
-	if (!paths)
-		return (FAILED_MALLOC);
-	i = 0;
-	while (paths[i])
-	{
-		paths[i] = strjoin_path_cmd(paths[i], data->cmds_block[block_id].commands[0]);
-		if (!paths[i])
-		{
-			ft_free((void **)paths, get_path_size(paths));
-			return (FAILED_MALLOC);
-		}
-		if (access(paths[i], X_OK) == 0)
-		{
-			printf(BLUE"command without paths? %s\n"END, data->cmds_block[block_id].commands[0]);
-			data->cmds_block[block_id].commands[0] = ft_strdup(paths[i]);
-			printf(BLUE"command with paths? %s\n"END, data->cmds_block[block_id].commands[0]);
-			ft_free((void **)paths, get_path_size(paths));
-			return (SUCCESS);
-		}
-		i++;
-	}
-	return (FAILURE);
-}
-*/
