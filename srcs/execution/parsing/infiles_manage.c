4e1d2cee7b307ba6ff4e3fb1545f58ceb96c21f3
bpoumeau <delafforest.brieuc@gmail.com>
2023-05-12 15:35:26 +0200

    infiles et outfile management

--- srcs/execution/parsing/infiles_manage.c
+++ srcs/execution/parsing/infiles_manage.c
@@ -27,6 +27,68 @@ bool	is_err_next_to_pipe(t_emt token)
 	return (true);
 }
 
+static t_return_status	_set_outfile(t_data *data, char **file, \
+									int block_id, char **env)
+{
+	char	**arr;
+	bool	signal;
+
+	signal = file_is_empty(*file);
+	check_opened_outfiles(data, block_id);
+	if (data->cmds_block[block_id].infile < 0
+		|| data->cmds_block[block_id].outfile < 0
+		|| data->cmds_block[block_id].fd_hd[0] < 0
+		|| data->cmds_block[block_id].is_ambiguous == true)
+		return (SUCCESS);
+	cut_line_on(*file, &arr);
+	join_arr_on(arr, file, env);
+	if (ft_strchr(*file, -32) != NULL || (**file == 0 && signal))
+	{
+		manage_ambiguous(&(data->cmds_block[block_id]), *file);
+		return (SUCCESS);
+	}
+	data->cmds_block[block_id].outfile = open(*file, O_WRONLY | O_CREAT | \
+	O_TRUNC, 0644);
+	if (data->cmds_block[block_id].outfile == -1)
+	{
+		g_ret_val = 1;
+		perror(*file);
+		return (FAILURE);
+	}
+	return (SUCCESS);
+}
+
+static t_return_status	_set_appends(t_data *data, char **file, \
+									int block_id, char **env)
+{
+	char	**arr;
+	bool	signal;
+
+	signal = file_is_empty(*file);
+	check_opened_outfiles(data, block_id);
+	if (data->cmds_block[block_id].infile < 0
+		|| data->cmds_block[block_id].outfile < 0
+		|| data->cmds_block[block_id].fd_hd[0] < 0
+		|| data->cmds_block[block_id].is_ambiguous == true)
+		return (SUCCESS);
+	cut_line_on(*file, &arr);
+	join_arr_on(arr, file, env);
+	if (ft_strchr(*file, -32) != NULL || (**file == 0 && signal))
+	{
+		manage_ambiguous(&(data->cmds_block[block_id]), *file);
+		return (SUCCESS);
+	}
+	data->cmds_block[block_id].outfile = open(*file, O_WRONLY | O_CREAT | \
+	O_APPEND, 0644);
+	if (data->cmds_block[block_id].outfile == -1)
+	{
+		g_ret_val = 1;
+		perror(*file);
+		return (FAILURE);
+	}
+	return (SUCCESS);
+}
+
 t_return_status	infiles_management(t_data *data, t_string_token *lst_of_tok, char **env)
 {
 	int				i;
@@ -47,6 +109,16 @@ t_return_status	infiles_management(t_data *data, t_string_token *lst_of_tok, cha
 			_set_heredoc(data, temp->content, i);
 			temp->content = NULL;
 		}
+		if (temp->token == CHEVRON_OT)
+		{
+			temp = temp->next;
+			_set_outfile(data, &(temp->content), i, env);
+		}
+		if (temp->token == APPENDS)
+		{
+			temp = temp->next;
+			_set_appends(data, &(temp->content), i, env);
+		}
 		if (temp->token == PIPE)
 			i++;
 		temp = temp->next;
@@ -61,10 +133,13 @@ static t_return_status	_set_infile(t_data *data, char **file, int block_id, char
 	char	**arr;
 	bool	signal;
 
-	if (data->cmds_block[block_id].infile == -1)
-		return (TAMERE);
 	check_opened_infiles(data, block_id);
 	signal = file_is_empty(*file);
+	if (data->cmds_block[block_id].infile < 0
+		|| data->cmds_block[block_id].outfile < 0
+		|| data->cmds_block[block_id].fd_hd[0] < 0
+		|| data->cmds_block[block_id].is_ambiguous == true)
+		return (SUCCESS);
 	cut_line_on(*file, &arr);
 	join_arr_on(arr, file, env);
 	if (ft_strchr(*file, -32) != NULL || (**file == 0 && signal))
@@ -90,6 +165,11 @@ static t_return_status	_set_infile(t_data *data, char **file, int block_id, char
 static t_return_status	_set_heredoc(t_data *data, char *hr_data, int block_id)
 {
 	check_opened_infiles(data, block_id);
+	if (data->cmds_block[block_id].infile < 0
+		|| data->cmds_block[block_id].outfile < 0
+		|| data->cmds_block[block_id].fd_hd[0] < 0
+		|| data->cmds_block[block_id].is_ambiguous == true)
+		return (SUCCESS);
 	if (data->cmds_block[block_id].is_heredoc)
 		free(data->cmds_block[block_id].heredoc_data);
 	data->cmds_block[block_id].is_heredoc = true;
