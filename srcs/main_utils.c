1c9661c0f3811e7aee66dbe5a7d0dfb63570fe04
bpoumeau <delafforest.brieuc@gmail.com>
2023-05-17 13:35:46 +0200

    youhou

--- srcs/main_utils.c
+++ srcs/main_utils.c
@@ -13,9 +13,9 @@
 #include "minishell_execution.h"
 #include "minishell_parsing.h"
 
-t_return_status	check_arguments(int ac, char **av)
+t_return_status	check_arguments(char **av)
 {
-	if (ac != 1)
+	if (av[1] != NULL)
 	{
 		ft_dprintf(2, "minishell: %s: ", av[1]);
 		ft_dprintf(2, "the arguments are not processed.\n");
