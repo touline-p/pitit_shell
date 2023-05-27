4ee2f5e52eb56c018d02bf0a57038737aed28b8e
bpoumeau <delafforest.brieuc@gmail.com>
2023-05-27 14:43:21 +0200

    norm libft

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: bpoumeau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 13:21:30 by bpoumeau          #+#    #+#             */
-/*   Updated: 2022/11/10 19:23:43 by bpoumeau         ###   ########lyon.fr   */
+/*   Updated: 2023/05/27 14:41:38 by bpoumeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	while (*s)
 		(*f)(i++, s++);
 }
-
 
 void	ft_arriteri(void **arr, void *(*f)(char **))
 {
