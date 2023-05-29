47f67b7ba6e8effa43bdc58fa74934ebc8370675
bpoumeau <delafforest.brieuc@gmail.com>
2023-05-29 16:43:56 +0200

    unset invalid identifier

--- sources/builtins/unset.c
+++ sources/builtins/unset.c
@@ -14,7 +14,8 @@
 
 /*---- prototypes ------------------------------------------------------------*/
 
-static	t_return_status	_unset_builtin_ep(char *msg);
+static t_return_status	_unset_builtin_ep(char *msg);
+static void				_is_valid_identifier(char *str);
 
 /*----------------------------------------------------------------------------*/
 
@@ -26,6 +27,7 @@ t_return_status	unset_builtin(char **args, char ***env_pt)
 	tmp = args + 1;
 	while (*tmp)
 	{
+		_is_valid_identifier(*tmp);
 		if (ft_strchr(*tmp, '=') != NULL)
 		{
 			tmp++;
@@ -37,12 +39,35 @@ t_return_status	unset_builtin(char **args, char ***env_pt)
 				return (_unset_builtin_ep("unset: malloc_failed\n"));
 		tmp++;
 	}
-	g_ret_val = 0;
 	return (SUCCESS);
 }
 
-static	t_return_status	_unset_builtin_ep(char *msg)
+static t_return_status	_unset_builtin_ep(char *msg)
 {
 	ft_dprintf(2, "%s", msg);
 	return (FAILED_MALLOC);
+}
+
+static void	_is_valid_identifier(char *str)
+{
+	bool flag;
+	char *tmp;
+
+	flag = false;
+	tmp = str;
+	while (*str)
+	{
+		if (ft_isalpha(*str) == false && *str != '_')
+		{
+			flag =true;
+			break ;
+		}
+		str++;
+	}
+	if (flag)
+	{
+		g_ret_val = 1;
+		ft_dprintf(2, \
+		"minishell: unset: '%s': not a valid identifier\n", tmp);
+	}
 }
